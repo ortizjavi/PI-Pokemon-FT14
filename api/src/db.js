225e57291d70fc076fc5c469f290ac4d25678bda
ortizjavi <ortizjavier160994@gmail.com>
2021-07-24 21:50:40 -0300

    adding config to sequelize connection

--- api/src/db.js
+++ api/src/db.js
@@ -11,6 +11,11 @@ const DATABASE = DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST
 const sequelize = new Sequelize(DATABASE, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+      ssl: true
+  },
+  dialect: 'postgres',
+  protocol: 'postgres'
 });
 const basename = path.basename(__filename);
 
