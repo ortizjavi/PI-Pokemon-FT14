596c1ce69689cdb354db58949a1d49fb461c2eb7
ortizjavi <ortizjavier160994@gmail.com>
2021-07-27 20:14:31 -0300

    fix devtools redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,7 +5,9 @@ import thunk from "redux-thunk";
 const store = createStore(
   rootReducer,
   compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+   window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f)
 );
 
 export default store;
