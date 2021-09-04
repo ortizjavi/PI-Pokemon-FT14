ed82209370181e7b2aac286585ebf0c12fc6fe9a
ortizjavi <ortizjavier160994@gmail.com>
2021-09-04 12:39:50 -0300

    new dom

--- api/src/app.js
+++ api/src/app.js
@@ -16,6 +16,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ortizjavi.github.io'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://18.116.48.160'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
